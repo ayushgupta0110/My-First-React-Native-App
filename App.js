56219cae3b422fce44e7ff43f21bf66a63a96386
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-09-19 11:28:27 +0530

    updated App Component

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+
+import Header from "./components/header";
+// import TodoForm from "./components/todo-form";
 
 export default function App() {
+  const [todos, setTodo] = useState([
+    { text: "create an app", key: "1" },
+    { text: "play chrome dino", key: "2" },
+    { text: "Buy vegis", key: "3" },
+    { text: "Complete Assignments", key: "4" },
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/*Header */}
+      <Header />
+      <View style={styles.content}>
+        {/* todo form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+      {/* Status bar */}
+      <StatusBar barStyle="light-content" />
     </View>
   );
 }
@@ -14,8 +35,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: "20",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  list: {
+    flex: 1,
+    justifyContent: "center",
   },
 });
