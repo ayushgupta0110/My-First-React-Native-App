e9c504ff091de000286beadba3d6a121c1f92ed1
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-09-19 11:28:39 +0530

    Added basic todoform component

---
+++ components/todoform.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function TodoForm(){
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+});    
